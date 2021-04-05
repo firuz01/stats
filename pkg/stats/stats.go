87e355ed88f46825d9171cef00e875a0c63fae3a
firuz.shermatov <firuz.shermatov@go.alif.academy>
2021-04-05 18:35:10 +0500

    The the Status of Payments was added to functions

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -1,7 +1,8 @@
 package stats
 
 import (
-	"github.com/firuz01/bank/pkg/types"
+	"github.com/firuz01/bank/v2/pkg/types"
+	
 
 )
 
@@ -9,6 +10,9 @@ func Avg(payments []types.Payment) types.Money {
 	numPayments := types.Money(len(payments))
 	sumPayments := types.Money(0)
 	for _, payment := range payments {
+		if payment.Status == types.StatusFail {
+			continue
+		} 
 		moneyAmount := payment.Amount
 		sumPayments += moneyAmount
 	}
@@ -19,7 +23,7 @@ func TotalInCategory(payments []types.Payment, category types.Category) types.Mo
 
 		sumPayments := types.Money(0)
 		for _, payment := range payments {
-			if payment.Category != category {
+			if payment.Category != category || payment.Status == types.StatusFail {
 				continue
 			}
 			sumPayments += payment.Amount
